bc0d634f722754c4828de7070c053efff2b07a2d
Porigon <tsukune398@gmail.com>
2021-03-25 23:26:30 +0900

    MainActivityからResultActivityに遷移させる

--- app/src/main/java/com/example/titfortat/MainActivity.kt
+++ app/src/main/java/com/example/titfortat/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.titfortat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * メイン画面.
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        confessButton.setOnClickListener {
+            val intent = Intent(application, ResultActivity::class.java)
+            startActivity(intent)
+        }
+
+        silenceButton.setOnClickListener {
+            val intent = Intent(application, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
