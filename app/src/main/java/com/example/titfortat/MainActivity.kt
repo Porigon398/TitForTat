2bd70ffe5470455311f5c5160149f824513cc518
Porigon <tsukune398@gmail.com>
2021-03-27 21:28:43 +0900

    尋問に5回答えるとResultActivityに遷移する

--- app/src/main/java/com/example/titfortat/MainActivity.kt
+++ app/src/main/java/com/example/titfortat/MainActivity.kt
@@ -9,18 +9,29 @@ import kotlinx.android.synthetic.main.activity_main.*
  * メイン画面.
  */
 class MainActivity : AppCompatActivity() {
+    /** 尋問に答えた回数 */
+    private var mAnswerCount = 0
+    /** 尋問に答えさせる回数 */
+    private val MAX_ANSWER_COUNT = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         confessButton.setOnClickListener {
-            val intent = Intent(application, ResultActivity::class.java)
-            startActivity(intent)
+            mAnswerCount++
+            if(mAnswerCount >= MAX_ANSWER_COUNT) {
+                val intent = Intent(application, ResultActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         silenceButton.setOnClickListener {
-            val intent = Intent(application, ResultActivity::class.java)
-            startActivity(intent)
+            mAnswerCount++
+            if(mAnswerCount >= MAX_ANSWER_COUNT) {
+                val intent = Intent(application, ResultActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
